57d6ed9ad7f4187e083c4d8be6f102d011434558
Andreas Jaeger <aj@suse.com>
2016-09-20 14:52:55 +0200

    Make example config file consistent
    Update the sample config file to use _key as well for the
    access_token_key - like all other entries.
    
    Change-Id: I8e561a3827bed72601babcb021d12b0a8ec8cad0

--- statusbot/bot.py
+++ statusbot/bot.py
@@ -38,7 +38,7 @@ url=http://eavesdrop.example.com/irclogs/%(chan)s/%(chan)s.%(date)s.log.html
 [twitter]
 consumer_key=consumer_key
 consumer_secret=consumer_secret
-access_token_key=access_token
+access_token_key=access_token_key
 access_token_secret=access_token_secret
 """
 
